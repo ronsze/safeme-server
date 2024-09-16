2683bbaf9ef4780205b6b864da7059b2d344c979
zeorns <ljg5846@gmail.com>
2024-09-17 07:55:23 +0900

    https수정

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const https = require('https');
 const app = express();
+const https = require('http').Server(app);
 const io = require('socket.io')(https);
+
+app.get("/", function(req, res) {
+  console.log("Hello");
+  res.send("Hello World");
+})
 
 io.on('connection', function (socket) {
   var roomID = ""
   socket.emit("connect")
-  console.log("connected");
+  console.log("connecte");
 
   socket.on('enterRoom', (room) => {
     roomID = room
@@ -23,8 +28,8 @@ io.on('connection', function (socket) {
   });
 });
 
-const port = process.env.PORT || 8000 //http default port
+const port = process.env.PORT || 8000
 
 app.listen(port, function () {
-  console.log("HTTP server listening on port " + port);
+  console.log("HTTPS server listening on port " + port);
 });
