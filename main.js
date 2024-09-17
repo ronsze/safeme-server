95b5d570c61944b571927ab83bacc69d5f1e8188
zeorns <ljg5846@gmail.com>
2024-09-17 09:16:42 +0900

    socket 이슈 수정

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 const express = require('express');
-const app = express();
-const https = require('http').Server(app);
-const io = require('socket.io')(https);
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 app.get("/", function(req, res) {
-  console.log("Hello");
   res.send("Hello World");
 })
 
 io.on('connection', function (socket) {
-  var roomID = ""
-  socket.emit("connect")
-  console.log("connecte");
-
-  socket.on('enterRoom', (room) => {
-    roomID = room
+  socket.roomID = ""
+  console.log("connect");
+ 
+  socket.on('enterRoom', (roomID) => {
+    socket.roomID = roomID
     socket.join(roomID)
     socket.emit("enteredRoom")
   });
 
   socket.on("sendR1", (r1) => {
-    io.to(roomID).emit("sendR1", r1)
+    io.to(socket.roomID).emit("sendR1", r1)
   });
 
   socket.on("sendR2", (r2) => {
-    io.to(roomID).emit("sendR2", r2)
+    io.to(socket.roomID).emit("sendR2", r2)
   });
 });
 
 const port = process.env.PORT || 8000
 
 app.listen(port, function () {
-  console.log("HTTPS server listening on port " + port);
+  console.log("server listening on port " + port);
 });
