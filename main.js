820989cd6603982a248bc24c1235fdefdc212e99
zeorns <ljg5846@gmail.com>
2024-09-17 10:04:13 +0900

    https to http

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const express = require('express');
-var app = express();
-var server = require('https').createServer(app);
-var io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const http = require('http').Server(app) 
+const io = require('socket.io')(http)
 
 app.get("/", function(req, res) {
   res.send("Hello World");
